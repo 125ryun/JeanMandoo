44b1f5250bb8aea652678501972f3321ab766981
125ryun <125ryun@gmail.com>
2023-11-21 10:30:18 +0900

    pos tagging 단위 텍스트 전체에서 문장별로 변경

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import csv
 import pandas as pd
 import konlpy
 from konlpy.tag import Hannanum
@@ -10,61 +9,65 @@ from konlpy.tag import Okt
 os.environ['JAVA_HOME'] = r'C:\Program Files\Java\jdk-21'
 print("set environment variable: ", 'JAVA_HOME' in os.environ)
 
-text = open('./data/document_01.txt', encoding='utf8').read()
-
 hannanum = Hannanum()
 kkma = Kkma()
 komoran = Komoran()
 okt = Okt()
+print("set module object")
 
-pos_tagged_hannanum = hannanum.pos(text)
-pos_tagged_kkma = kkma.pos(text)
-pos_tagged_komoran = komoran.pos(text)
-pos_tagged_okt = okt.pos(text)
+# for file in os.scandir('./out'):
+#     os.remove(file.path)
 
-for file in os.scandir('./out'):
-    os.remove(file.path)
+def pos_tagging():
+    print("start pos tagging")
 
-out_file = open('./out/out_hannanum.txt', 'w', encoding='utf8')
-for item in pos_tagged_hannanum:
-    out_file.write(item[0] + "/" + item[1] + "\n")
-out_file.close()
+    # initialize data frame
+    result = pd.DataFrame()
+    tmp = pd.DataFrame()
 
-out_file = open('./out/out_kkma.txt', 'w', encoding='utf8')
-for item in pos_tagged_kkma:
-    out_file.write(item[0] + "/" + item[1] + "\n")
-out_file.close()
+    # data 텍스트를 문장들의 리스트로 불러들이기
+    text = open('./data/document_01.txt', encoding='utf8').readlines()
 
-out_file = open('./out/out_komoran.txt', 'w', encoding='utf8')
-for item in pos_tagged_komoran:
-    out_file.write(item[0] + "/" + item[1] + "\n")
-out_file.close()
+    # text의 각 문장 line에 대해 pos tagging 실시 후 기존 데이터에 이어 붙이기
+    for line in text:
+        # do pos tagging
+        pos_tagged_hannanum = hannanum.pos(line, ntags=22)
+        pos_tagged_kkma = kkma.pos(line)
+        pos_tagged_komoran = komoran.pos(line)
+        pos_tagged_okt = okt.pos(line)
 
-out_file = open('./out/out_okt.txt', 'w', encoding='utf8')
-for item in pos_tagged_okt:
-    out_file.write(item[0] + "/" + item[1] + "\n")
-out_file.close()
+        # write out file(txt)
+        out_file = open('./out/out_hannanum.txt', 'w', encoding='utf8')
+        for item in pos_tagged_hannanum:
+            out_file.write(item[0] + "/" + item[1] + "\n")
+        out_file.close()
 
-# with open('out.csv', 'w', newline='') as csvfile:
-#     spamwriter = csv.writer(csvfile, delimiter=' ',
-#                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-#     spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
-#     spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+        out_file = open('./out/out_kkma.txt', 'w', encoding='utf8')
+        for item in pos_tagged_kkma:
+            out_file.write(item[0] + "/" + item[1] + "\n")
+        out_file.close()
 
-pt_hannanum = pd.DataFrame({'hannanum':pos_tagged_hannanum})
-pt_kkma = pd.DataFrame({'kkma':pos_tagged_kkma})
-pt_komoran = pd.DataFrame({'komoran':pos_tagged_komoran})
-pt_okt = pd.DataFrame({'okt':pos_tagged_okt})
+        out_file = open('./out/out_komoran.txt', 'w', encoding='utf8')
+        for item in pos_tagged_komoran:
+            out_file.write(item[0] + "/" + item[1] + "\n")
+        out_file.close()
 
-# print(pt_hannanum)
+        out_file = open('./out/out_okt.txt', 'w', encoding='utf8')
+        for item in pos_tagged_okt:
+            out_file.write(item[0] + "/" + item[1] + "\n")
+        out_file.close()
 
-# result = pd.concat([pt_hannanum, pt_kkma, pt_komoran, pt_okt])
-result = pd.concat([pt_hannanum, pt_kkma, pt_komoran, pt_okt], axis=1)
-# result = pt_hannanum
-# result.join(pt_kkma)
-# result.join(pt_komoran)
-# result.join(pt_okt)
-print(result)
-print(result['kkma'][0]) # 'kkma'열 0행
-# result.to_csv('./out/result.csv', sep=',', na_rep='-', encoding='utf-8-sig')
-# out_csv = open('./out/result.csv', 'w')
+        pt_hannanum = pd.DataFrame({'hannanum':pos_tagged_hannanum})
+        pt_kkma = pd.DataFrame({'kkma':pos_tagged_kkma})
+        pt_komoran = pd.DataFrame({'komoran':pos_tagged_komoran})
+        pt_okt = pd.DataFrame({'okt':pos_tagged_okt})
+
+        tmp = pd.concat([pt_hannanum, pt_kkma, pt_komoran, pt_okt], axis=1)
+        result = pd.concat([result, tmp])
+    
+    # write out file(csv)
+    result.to_csv('./out/result_02.csv', sep=',', na_rep='-', encoding='utf-8-sig')
+
+    print("**** DONE ****")
+
+pos_tagging()
