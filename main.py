bf2234cc59b8e77baf9e11fa3f20ec105a3a8b90
125ryun <125ryun@gmail.com>
2023-12-27 06:11:41 +0900

    score calc module 하나로 합침

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 # import modules
 import os
-from modules import format, convert, metadata, s2
+from modules import format, convert, metadata, score
 
 ROOT_PATH = os.getcwd()
 format.format_syntax_rank()
 
-# # video -> audio(speech) -> text
+# video -> audio(speech) -> text
 convert.vid_to_aud()
 convert.aud_to_txt()
 
-# # # video(speech), text 메타 데이터 연산
+# video(speech), text 메타 데이터 연산
 metadata.calc_vid_len() # 영상 길이(min)의 분자, 분모를 차례대로 각 줄에 저장
 metadata.calc_txt_len() # 텍스트 어절 수를 저장
 
-# # 평가 준거별로 점수 산출
+# 평가 준거별로 점수 산출
 
-# # 음운 변동
+## 발화 속도(spm)
+score.score_spm()
 
-# # 발화 속도(spm)
-s2.score_spm()
+## 통사적 복잡성
+#score.tagpos(ROOT_PATH)
 
-# 통사적 복잡성
-#s3.score_syntax(ROOT_PATH)
+## 동음이의어, 다의어 출현
 
 # 최종 점수 산출
--- modules/score.py
+++ modules/score.py
@@ -5,7 +5,34 @@ import pandas as pd
 from ctypes import *
 import modules.utagger.bin.utagger as ut
 
-def score_syntax(root_path):
+def score_spm():
+    vid_dir = "data/vid"
+    out_dir = "out"
+
+    metadata_dir = "bin/metadata"
+
+    for file in os.listdir("bin/txt"):
+        vid_name, vid_ext = file.split(".")
+        output_path = f"{out_dir}/{vid_name}_score_spm.txt"
+        if os.path.exists(output_path):
+            continue
+        
+        with open(f"{metadata_dir}/{vid_name}_vid.txt", "r", encoding='UTF8') as f:
+            fraction = f.readlines()
+            num = int(fraction[0].strip())
+            denom = int(fraction[1])
+
+        with open(f"{metadata_dir}/{vid_name}_txt.txt", "r", encoding='UTF8') as f:
+            syllables = int(f.readline())
+
+        score = float(syllables / (num / denom))
+
+        with open(output_path, "w", encoding='UTF8') as f:
+            f.write(str(score))
+            
+        print(f"**** score(spm) 계산 완료: {output_path}")
+        
+def tagpos(root_path):
     
     tagger, lib = ut.Init_Utagger(root_path)
 
@@ -15,7 +42,7 @@ def score_syntax(root_path):
         txt_name, txt_ext = txt_file.split(".")
         txt_path = f"{txt_dir}/{txt_file}"
         output_path = f"{out_dir}/{txt_name}.txt"
-        if os.path.exists(f"{out_dir}/{txt_name}.txt"):
+        if os.path.exists(output_path):
             continue
         
         with open(txt_path, "r", encoding='utf-8') as f:
